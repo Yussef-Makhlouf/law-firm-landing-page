a5bf3bef5d876bd7c429c2767525400c11be599c
Yussef Makhlouf <yussef.ali.it@gmail.com>
2025-12-02 23:20:18 +0200

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -12,7 +12,7 @@ export function HeroSection() {
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: "url('/hero2.jpeg')",
+          backgroundImage: "url('/on-air.jpeg')",
         }}
       />
       
